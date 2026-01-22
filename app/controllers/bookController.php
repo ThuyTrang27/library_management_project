9181628dfac809dd9967b2b37ee11cfe17d05728
ThuyTrang27 <nguyenthuytrang2021bd@gmail.com>
2026-01-22 20:50:20 +0700

    bookrequest

---
+++ app/controllers/bookController.php
@@ -0,0 +1,26 @@
+<?php
+class BookController
+{
+    private $bookModel;
+
+    public function __construct($db)
+    {
+        require_once __DIR__ . '/../models/book.php';
+        $this->bookModel = new Book($db);
+    }
+
+    public function showListBook()
+    {
+        $limit = 20;
+        $currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        if ($currentPage < 1) $currentPage = 1;
+        $offset = ($currentPage - 1) * $limit;
+
+        $books = $this->bookModel->getBooksPagination($limit, $offset);
+        $totalBooks = $this->bookModel->getTotalBooks();
+        $totalPages = ceil($totalBooks / $limit);
+
+        require_once __DIR__ . '/../views/books/bookListView.php';
+    }
+}
+
