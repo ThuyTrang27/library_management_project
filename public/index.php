a52fc0e265f8ba1d8ad2d2e75cf08da22542ecfd
Anh <ngocanhqb123end@gmail.com>
2026-01-25 21:46:04 +0700

    done category

--- public/index.php
+++ public/index.php
@@ -1,32 +1,55 @@
 <?php
-// require_once __DIR__ . '/../config/config.php';
-require_once dirname(__DIR__) . '/config/config.php';
-
-require_once dirname(__DIR__) . '/app/models/user.php';
-require_once dirname(__DIR__) . '/app/models/book.php';
-
-require_once dirname(__DIR__) . '/app/controllers/authController.php';
-require_once dirname(__DIR__) . '/app/controllers/bookController.php';
-require_once dirname(__DIR__) . '/app/models/category.php';
-
-
-
 session_start();
 
-$database = new Database();
-$db = $database->connect();
-$categoryModel = new Category($db);
-$categories = $categoryModel->getAllCategories();
+/*
+|--------------------------------------------------------------------------
+| CONFIG & DATABASE
+|--------------------------------------------------------------------------
+*/
+require_once dirname(__DIR__) . '/config/config.php';
 
-$authController = new AuthController(new User($db));
-$bookController = new BookController($db);
+/*
+|--------------------------------------------------------------------------
+| MODELS
+|--------------------------------------------------------------------------
+*/
+require_once dirname(__DIR__) . '/app/models/user.php';
+require_once dirname(__DIR__) . '/app/models/book.php';
 require_once dirname(__DIR__) . '/app/models/category.php';
 
+/*
+|--------------------------------------------------------------------------
+| CONTROLLERS
+|--------------------------------------------------------------------------
+*/
+require_once dirname(__DIR__) . '/app/controllers/authController.php';
+require_once dirname(__DIR__) . '/app/controllers/bookController.php';
 
+/*
+|--------------------------------------------------------------------------
+| DATABASE CONNECTION
+|--------------------------------------------------------------------------
+*/
+$database = new Database();
+$db = $database->connect();
 
+/*
+|--------------------------------------------------------------------------
+| CONTROLLER INSTANCES
+|--------------------------------------------------------------------------
+*/
+$authController = new AuthController(new User($db));
+$bookController = new BookController($db);
+
+/*
+|--------------------------------------------------------------------------
+| ROUTING
+|--------------------------------------------------------------------------
+*/
 $action = isset($_GET['action']) ? $_GET['action'] : 'listbook';
 
 switch ($action) {
+
     case 'register':
         $authController->registerView();
         break;
@@ -34,7 +57,6 @@ switch ($action) {
     case 'doregister':
         $authController->doRegister();
         break;
-
 
     case 'login':
         $authController->login();
@@ -48,9 +70,10 @@ switch ($action) {
     case 'listbook':
         $bookController->showListBook();
         break;
-    case 'category':
-        $bookController->showByCategory();
-        break;
+
+    // case 'category':
+    //     $bookController->showByCategory();
+    //     break;
 
     default:
         header("Location: index.php?action=listbook");
